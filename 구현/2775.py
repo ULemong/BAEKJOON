8e93c60a88261bd97ee0842232e89c54038c2c62
ULemong <86288617+ULemong@users.noreply.github.com>
2021-07-23 19:38:54 +0900

    Update 2775.py
    부녀회장이 될테야

--- 구현/2775.py
+++ 구현/2775.py
@@ -1,17 +1,16 @@
-k = 2
-n = 3
-a = []
-new = []
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    data = []
 
-for i in range(k):
-    if i == 0:
-        for k in range(n):
-            a.append(k+1)
-        continue
-    for j in range(1, n+1):
-        new.append(sum(a[0:j]))
-    a = new
-    new = []
-    print(a)
+    for i in range(k):
+        new = []
+        if i == 0:
+            for k in range(n):
+                data.append(k+1)
+            continue
+        for j in range(1, n+1):
+            new.append(sum(data[0:j]))
+        data = new
 
-print(sum(a))
+    print(sum(data))
