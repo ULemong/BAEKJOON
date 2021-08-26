b6533db36570196abc06a1e1f677a1519bb3a035
ULemong <maintain9912@gmail.com>
2021-08-26 21:07:07 +0900

    프린터 큐

--- 큐/1966.py
+++ 큐/1966.py
@@ -1,20 +1,18 @@
 for _ in range(int(input())):
-    data = []
-    count = 0
     N, M = map(int, input().split())
-    importent = list(map(int, input().split()))
-    while count <= len(importent):
-        count += 1
+    data = map(int, input().split())
 
-        if importent[M] == N:
-            break
-        N -= 1
+    data = [(value, index) for index, value in enumerate(data)]
+    count = 0
 
-
-
-        if importent[M] == max(importent):
+    while True:
+        if data[0][0] == max(data, key=lambda x: x[0])[0]:
             count += 1
-            break
+            if M == data[0][1]:
+                break
+            else:
+                data.pop(0)
         else:
+            data.append(data.pop(0))
 
-
+    print(count)
