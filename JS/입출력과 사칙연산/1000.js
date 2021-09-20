c333a9e5b9542578c3007fdeb90ce54dfd1bfc76
ULemong <86288617+ULemong@users.noreply.github.com>
2021-09-21 02:11:15 +0900

    A+B

--- JS/입출력과 사칙연산/1000.js
+++ JS/입출력과 사칙연산/1000.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
-var input = fs.readFileSync("/dev/stdin").toString().split(' ');
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split(' ');
 
 console.log(Number(input[0]) + Number(input[1]));
