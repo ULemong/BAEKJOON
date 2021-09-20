9cb5f067707442cc021e5de5e0142e9e549339b0
ULemong <86288617+ULemong@users.noreply.github.com>
2021-09-21 02:13:29 +0900

    사칙연산

--- JS/입출력과 사칙연산/10869.js
+++ JS/입출력과 사칙연산/10869.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
 
 console.log(input[0] + input[1]);
 console.log(input[0] - input[1]);
