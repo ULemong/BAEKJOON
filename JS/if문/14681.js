82713ca2b9efa66e0e5eba6d2a4f24c7c66bae80
ULemong <86288617+ULemong@users.noreply.github.com>
2021-09-22 18:29:20 +0900

    사분면 고르기

---
+++ JS/if문/14681.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  if (input[0] > 0) {
+    if (input[1] > 0) {
+      console.log(1);
+    }
+    else {
+      console.log(4);
+    }
+  } else {
+    if (input[1] > 0) {
+      console.log(2);
+    }
+    else {
+      console.log(3);
+    }
+  }
+  process.exit();
+});
