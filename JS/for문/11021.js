f53336e2afb927d20a80b435b8d4f49baab4f5e2
ULemong <86288617+ULemong@users.noreply.github.com>
2021-09-24 22:42:54 +0900

    A+B - 7

---
+++ JS/for문/11021.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let answer = '';
+
+for (let i=1; i<=input[0]; i++) {
+  const num = input[i].split(' ').map(Number);
+  answer += (`Case #${i}: ${num[0]+num[1]}`+ '\n');
+}
+console.log(answer);
